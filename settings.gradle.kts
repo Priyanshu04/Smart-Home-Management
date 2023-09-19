66a3e55d9e22a968a8bafbee630a350f3fe566ca
Priyanshu_11 <34536868+Priyanshu04@users.noreply.github.com>
2023-09-19 20:24:31 +0530

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SHM_Page2"
+include(":app")
+ 
